073f667576f65d5c2bd22d60d1c7b46462d052ad
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2023-05-28 17:24:29 +0200

    Create CHHub.lua

---
+++ CHHub.lua
@@ -0,0 +1,7 @@
+local ids = {
+189707
+}
+
+if game.PlaceId == ids[1] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/HPHub/main/NaturalDisasterSurvival.lua"))()
+end
