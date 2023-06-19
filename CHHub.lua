380f1beb40f8abf9385d89a4297e75244c283fd5
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2023-06-19 11:03:04 +0200

    Update CHHub.lua

--- CHHub.lua
+++ CHHub.lua
@@ -2,7 +2,9 @@ local ids = {
     189707, --Natural Disaster Survival
     192800, --Work at a pizza place
     8888615802, --Rainbow friends chapter 1  
-    13622981808 --Rainbow friends chapter 2
+    13622981808, --Rainbow friends chapter 2
+    815405518, --The floor is lava
+    11474660364 --Natural Disaster Survival With Free Items
 }
 
 if game.PlaceId == ids[1] then
@@ -13,4 +15,10 @@ elseif game.PlaceId == ids[3] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/rainbow%20friends%20chapter%201.lua"))()
 elseif game.PlaceId == ids[4] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/rainbow%20friends%20chapter%202.lua"))()
+elseif game.PlaceId == ids[5] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/The%20floor%20is%20lava.lua"))()
+elseif game.PlaceId == ids[6] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/Natural%20Disaster%20Survival.lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/Universal.lua"))()
 end
